7941f7b4bbfc7a81de4fa9b19c9ff3272454383c
prem <premrajj53@gmail.com>
2026-03-18 16:00:47 +0530

    ADDED USER INPUT TO SET ALARM

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main (String []args){
+import java.time.DateTimeException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
+public class Main {
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:MM:SS");
+        LocalTime alarmTime;
+        try {
+            System.out.print("Enter an alarm time (HH:MM:SS) :");
+            String imputTime =  sc.nextLine();
+
+            alarmTime = LocalTime.parse(imputTime,format);
+            System.out.println("Alarm set for " + alarmTime);
+        }
+       catch (DateTimeException e){
+           System.out.println("Invalid format! USE HH:MM:SS");
+
+       }
     }
 }
